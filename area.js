00ef25e0e9869b8a6d42a32e3dd1b04a47e92718
NamLinish <namithlinish@gmail.com>
2026-04-06 14:31:14 +0530

    Create area.js

---
+++ area.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rectangle Area</title>
+</head>
+<body>
+
+<h2>Area of Rectangle</h2>
+
+<script>
+    let length = prompt("Enter length:");
+    let width = prompt("Enter width:");
+
+    let area = length * width;
+
+    alert("Area of Rectangle = " + area);
+</script>
+
+</body>
+</html>
